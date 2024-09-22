c252218b86e106631691286f8d0606f1f8a33e34
Jack Rong <jackrong03@gmail.com>
2024-09-22 23:10:18 +0100

    add toolbar group for buttons

--- PomodoroTimer Watch App/Views/PomodoroView.swift
+++ PomodoroTimer Watch App/Views/PomodoroView.swift
@@ -16,8 +16,15 @@ struct PomodoroView: View {
     var body: some View {
         NavigationStack {
             VStack {
+                Text(pomodoroViewModel.currentSession)
+                    .font(.headline.bold())
+                    .foregroundStyle(.secondary)
+                
+                Spacer()
+                    
                 Text(pomodoroViewModel.formattedRemainingTime)
                     .font(.system(size: 60))
+                    .foregroundStyle(.primary)
                 
                 Spacer()
                 
@@ -26,61 +33,61 @@ struct PomodoroView: View {
                         if number < pomodoroViewModel.currentSessionsDone {
                             Image(systemName: "circle.fill")
                                 .font(.caption)
+                                .foregroundStyle(.secondary)
                         } else if number == pomodoroViewModel.currentSessionsDone && pomodoroViewModel.isWorkSession {
                             Image(systemName: "circle.dotted.circle")
                                 .font(.caption)
+                                .foregroundStyle(.secondary)
                         } else {
                             Image(systemName: "circle.dotted")
                                 .font(.caption)
+                                .foregroundStyle(.secondary)
                         }
                     }
                 }
-
+                
                 Spacer()
                 Spacer()
                 Spacer()
                 
-                HStack {
-                    Button(action: {
-                        if pomodoroViewModel.isTimerTicking {
-                            pomodoroViewModel.pauseTimer()
-                            alertsViewModel.playPressedHaptic()
-                        } else {
-                            pomodoroViewModel.startTimer()
-                            alertsViewModel.playStartHaptic()
+                .toolbar {
+                    ToolbarItemGroup(placement: .bottomBar) {
+                        Button(action: {
+                            if pomodoroViewModel.isTimerTicking {
+                                pomodoroViewModel.pauseTimer()
+                                alertsViewModel.playPressedHaptic()
+                            } else {
+                                pomodoroViewModel.startTimer()
+                                alertsViewModel.playStartHaptic()
+                            }
+                        }) {
+                            Image(systemName: pomodoroViewModel.isTimerTicking ? "pause.fill" : "play.fill")
                         }
-                    }) {
-                        Image(systemName: pomodoroViewModel.isTimerTicking ? "pause.fill" : "play.fill")
-                    }
-                    
-                    Button(action: {
-                        pomodoroViewModel.endCycle()
-                        alertsViewModel.playPressedHaptic()
-                    }) {
-                        Image(systemName: "stop.fill")
-                    }
-                    
-                    Button(action: {
-                        pomodoroViewModel.resetTimer()
-                        alertsViewModel.playPressedHaptic()
-                    }) {
-                        Image(systemName: "arrow.circlepath")
-                    }
-                    
-                    Button(action: {
-                        pomodoroViewModel.skipSession()
-                        alertsViewModel.playPressedHaptic()
-                    }) {
-                        Image(systemName: "forward.end.fill")
+                        
+                        Button(action: {
+                            pomodoroViewModel.endCycle()
+                            alertsViewModel.playPressedHaptic()
+                        }) {
+                            Image(systemName: "stop.fill")
+                        }
+                        
+                        Button(action: {
+                            pomodoroViewModel.resetTimer()
+                            alertsViewModel.playPressedHaptic()
+                        }) {
+                            Image(systemName: "arrow.circlepath")
+                        }
+                        
+                        Button(action: {
+                            pomodoroViewModel.skipSession()
+                            alertsViewModel.playPressedHaptic()
+                        }) {
+                            Image(systemName: "forward.end.fill")
+                        }
+
                     }
                 }
-            }
-            .toolbar {
-                ToolbarItem(placement: .topBarLeading) {
-                    Text(pomodoroViewModel.currentSession)
-                        .font(.headline.bold())
-                        .foregroundStyle(.secondary)
-                }
+
             }
         }
         .onChange(of: alertsViewModel.showAlert) { _, newValue in
