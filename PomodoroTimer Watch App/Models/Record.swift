6b325d3d548884a891b94e9f533ef7cea4e56fe6
Jack Rong <jackrong03@gmail.com>
2024-10-05 02:11:49 +0100

    add Record model

---
+++ PomodoroTimer Watch App/Models/Record.swift
@@ -0,0 +1,28 @@
+//
+//  Record.swift
+//  PomodoroTimer
+//
+//  Created by Jack Rong on 05/10/2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Record {
+    @Attribute(.unique) var date: Date
+    var workSessionsCompleted: Int
+    var dailyTarget: Int
+    var isDailyTargetMet: Bool
+    
+    init(date: Date, workSessionsCompleted: Int, dailyTarget: Int, isDailyTargetMet: Bool) {
+        self.date = date
+        self.workSessionsCompleted = workSessionsCompleted
+        self.dailyTarget = dailyTarget
+        self.isDailyTargetMet = isDailyTargetMet
+    }
+    
+    convenience init() {
+        self.init(date: Date.now, workSessionsCompleted: 0, dailyTarget: NumberSetting.dailyTarget.currentValue, isDailyTargetMet: false)
+    }
+}
